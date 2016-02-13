f31decff17d516e35c759c8447b1da742b1a165d
Joost de Vries <joost@ziener.org>
2016-02-13 19:40:22 +0100

    adds convenience setting for tslint-microsoft-contrib

--- sbt-tslint-plugin-tester/build.sbt
+++ sbt-tslint-plugin-tester/build.sbt
@@ -1,14 +1,16 @@
 import com.typesafe.sbt.web.Import.WebKeys._
+
 lazy val root = (project in file(".")).enablePlugins(SbtWeb)
 
 incOptions := incOptions.value.withNameHashing(true)
 updateOptions := updateOptions.value.withCachedResolution(cachedResoluton = true)
 
-libraryDependencies ++=Seq(   "org.webjars.npm" % "tslint-eslint-rules" % "1.0.1")
+libraryDependencies ++= Seq("org.webjars.npm" % "tslint-eslint-rules" % "1.0.1",
+  "org.webjars.npm" % "tslint-microsoft-contrib" % "2.0.1")
 
 dependencyOverrides += "org.webjars.npm" % "minimatch" % "2.0.10"
 
-(formatter in tslint) := Some("json")
+//(formatter in tslint) := Some("json")
 
-(rulesDirectories in tslint) := Some(List((tslintEslintRulesDir).value))
+(rulesDirectories in tslint) := Some(List(tslintEslintRulesDir.value, tslintMsContribRulesDir.value))
 JsEngineKeys.engineType := JsEngineKeys.EngineType.Node
--- src/main/scala/name/devries/tslint/SbtTSLint.scala
+++ src/main/scala/name/devries/tslint/SbtTSLint.scala
@@ -18,13 +18,19 @@ object SbtTSLint extends AutoPlugin {
 
     val tslint = TaskKey[Seq[File]]("tslint", "Perform Typescript linting.")
 
-    val config = SettingKey[Option[File]]("tslint-config", "The location of a JSHint configuration file. Default is ./tslint.json")
+    val config = SettingKey[Option[File]]("tslint-config",
+      "The location of a JSHint configuration file. Default is ./tslint.json")
     val formatter = SettingKey[Option[String]]("tslint-formatter", "The tslint formatter. Default is: prose")
     val formattersDirectory = SettingKey[Option[String]]("tslint-formatters-dir", "The directory for tslint formatters. Not required.")
-    val rulesDirectories = SettingKey[Option[List[String]]]("tslint-rules-dirs", "The zero or more directories of rules directories. Default is empty list.")
-    val resolvedConfig = TaskKey[Option[File]]("tslint-resolved-config", "The actual location of a tslint configuration file if present. If tslint-config is none then the task will seek a tslint.json in the project folder. If that's not found then tslint.json will be searched for in the user's home folder.")
+    val rulesDirectories = SettingKey[Option[List[String]]]("tslint-rules-dirs",
+      "The zero or more directories of rules directories. Default is empty list.")
+    val resolvedConfig = TaskKey[Option[File]]("tslint-resolved-config",
+      "The actual location of a tslint configuration file if present. If tslint-config is none then the task will seek a tslint.json in the project folder. If that's not found then tslint.json will be searched for in the user's home folder.")
 
-    val tslintEslintRulesDir = SettingKey[String]("tslint-eslint-rules-dir","The directory of the eslint extension of tslint rules if the tslint-eslint-rules npm webjar is added as a dependency to the build")
+    val tslintEslintRulesDir = SettingKey[String]("tslint-eslint-rules-dir",
+      "The directory of the eslint extension of tslint rules if the tslint-eslint-rules npm webjar is added as a dependency to the build")
+    val tslintMsContribRulesDir = SettingKey[String]("tslint-ms-contrib-rules-dir",
+      "The directory of the tslint microsoft contrib rules if the tslint-microsoft-contrib npm webjar is added as a dependency to the build")
   }
 
   import SbtWeb.autoImport._
@@ -82,7 +88,8 @@ object SbtTSLint extends AutoPlugin {
   }
 
   override def projectSettings = Seq(
-    tslintEslintRulesDir := ((webJarsNodeModulesDirectory in Assets).value / "tslint-eslint-rules" / "dist" /"rules").getCanonicalPath
+    tslintEslintRulesDir := ((webJarsNodeModulesDirectory in Assets).value / "tslint-eslint-rules" / "dist" /"rules").getCanonicalPath,
+    tslintMsContribRulesDir := ((webJarsNodeModulesDirectory in Assets).value / "tslint-microsoft-contrib").getCanonicalPath
 
   ) ++ inTask(tslint)(
     SbtJsTask.jsTaskSpecificUnscopedSettings ++
